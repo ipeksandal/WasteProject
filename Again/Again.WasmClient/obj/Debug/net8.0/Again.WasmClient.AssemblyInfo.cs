8ba35e59fb04b18e80634e83e74504148dcee27b
İpek Sandal <ipekksandalll@gmail.com>
2024-07-27 19:26:04 +0300

    new

--- Again/Again.WasmClient/obj/Debug/net8.0/Again.WasmClient.AssemblyInfo.cs
+++ Again/Again.WasmClient/obj/Debug/net8.0/Again.WasmClient.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Again.WasmClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5096913ea8655fad964d0857eab1b7ece4ff279")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c6f8dd1d4f18d6504937be74e65bf61d8a94490")]
 [assembly: System.Reflection.AssemblyProductAttribute("Again.WasmClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Again.WasmClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
