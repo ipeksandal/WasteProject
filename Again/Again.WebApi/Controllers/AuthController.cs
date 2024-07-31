3581f8bef8078229b18a3ddbddbac412d2a3fb47
İpek Sandal <ipekksandalll@gmail.com>
2024-07-31 17:34:22 +0300

    authController added

---
+++ Again/Again.WebApi/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+using Again.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Again.WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly AuthService _authService;
+
+    public AuthController(AuthService authService)
+    {
+        _authService = authService;
+    }
+}
